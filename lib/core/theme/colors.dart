a350f7c7fea3b81c949927e57ee2996f9d0fbb7d
CACL1000 <leon.camiloa23@gmail.com>
2025-10-30 10:26:48 -0300

    subiendo el proyecto a github

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6C63FF);
+  static const Color background = Colors.white;
+  static const Color text = Colors.black87;
+  static const Color textLight = Colors.white;
+  static const Color success = Color(0xFF4CAF50);
+  static const Color error = Colors.red;
+  static const Color buttonBackground = Color(0xFF6C63FF);
+  static const Color navigationBarBackground = Color(0xFF6C63FF);
+}
